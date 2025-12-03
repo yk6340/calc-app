61b51f5167767a03c05a8ef65fa7a73db1978ddc
yk6340 <akira_u58h@yahoo.co.jp>
2025-12-03 21:41:56 +0900

    op変換

--- resources/views/livewire/calc.blade.php
+++ resources/views/livewire/calc.blade.php
@@ -8,6 +8,24 @@ state(['number1', 'op', 'number2']);
 
 <div>
     <h1>テスト表示</h1>
-    <p>{{ $number1 }} {{ $op }} {{ $number2 }}</p>
+
+    @php
+        $symbol = '?';
+
+        if ($op === 'addition') {
+            $symbol = '+';
+        } elseif ($op === 'subtraction') {
+            $symbol = '-';
+        } elseif ($op === 'multiplication') {
+            $symbol = '*';
+        } elseif ($op === 'division') {
+            $symbol = '/';
+        } else {
+            $symbol = '?';
+        }
+    @endphp
+
+    <p>{{ $number1 }} {{ $symbol }} {{ $number2 }}</p>
 </div>
+
 
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use Livewire\Volt\Volt;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Volt::route('/calcs/{number1}/{op}/{number2}', 'calc');
